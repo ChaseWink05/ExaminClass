c4e70f341214eed68770c2244a08c291da8f0355
ChaseWink05 <155601183+ChaseWink05@users.noreply.github.com>
2024-04-30 18:01:18 -0400

    Here is the updated file that I completed before Thursday that has all of corrections to. I simply had to make the treatCounter Static

--- VRFinalProject-main/Assets/ScoreManager.cs
+++ VRFinalProject-main/Assets/ScoreManager.cs
@@ -3,23 +3,54 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+
 //Michael
 
 public class ScoreManager : MonoBehaviour
 {
+    public TextManager textManager;
+    public int treatCounter = 0;
     public int score = 0;
     public Text Score;
+    
+    
 
     public void IncrementScore()
     {
         score += 1;
         UpdateScoreDisplay();
-        if (score == 5)
+        if (score == 10)
         {
             LoadNextScene();
         }
 
     }
+
+    /*public void dog()
+    {
+        void OnCollisionEnter(Collision collision)
+        {
+            if (collision.gameObject.name == "Dog_001" && treatCounter <= 3)
+            {
+                treatCounter++; // Increment the treat counter
+                
+
+                if (treatCounter == 3) // If the dog has received 3 treats 
+                {
+                    textManager.SetText("Enough, Stop");
+                    // Disable the colliders instead of destroying the treat object
+                    treatCollider.enabled = false;
+                    treatCollider2.enabled = false;
+                    return; // Exit the method to prevent further execution
+                }
+
+                // Don't destroy the treat object here
+                Destroy(gameObject);
+                textManager.SetText("Bark Bark");
+                
+            }
+        }
+    }*/
     void Start()
     {
         UpdateScoreDisplay();
--- VRFinalProject-main/Assets/TreatScript.cs
+++ VRFinalProject-main/Assets/TreatScript.cs
@@ -4,48 +4,52 @@ using UnityEngine;
 
 public class TreatScript : MonoBehaviour
 {
-    //Creating a variable to keep track of how many treats the dog has consumed
-    public int treatCounter;
+    //Creating a static variable to keep track of how many treats the dog has consumed throughout all of the scripts
+    public static int treatCounter = 0;
     public GameObject Treat;
     public TextManager textManager;
     public ScoreManager scoreManager;
-    //Storing both of the colliders as variables 
-    private Collider treatCollider;
-    private Collider treatCollider2;
+    //Storing both of the colliders as variables as static so it goes to all the scripts that have this script on it 
+    public static Collider treatCapsuleCollider;
+    public static Collider treatSphereCollider;
     
 
     void Start()
     {
         
         treatCounter = 0;
-        treatCollider = GetComponent<CapsuleCollider>(); //Taking both of the colliders off of the treat object so it cannot collide which destroys the object
-        treatCollider2 = GetComponent<SphereCollider>();
+        treatCapsuleCollider = GetComponent<CapsuleCollider>(); //Taking both of the colliders off of the treat object so it cannot collide which destroys the object
+        treatSphereCollider = GetComponent<SphereCollider>();
 
     }
 
     void OnCollisionEnter(Collision collision)
     {
-        if (collision.gameObject.name == "Dog_001")
+        if (collision.gameObject.name == "Dog_001" && treatCounter < 3)
         {
-            //treatCounter += 1; 
-            treatCounter = treatCounter + 1; //Incrementing the treatCounter to keep track of how many times the treat is given to the dog
-            Debug.Log(treatCounter); //testing to see if it is counting the treat
-            if (treatCounter == 3) //Conditional so when the dog has recived 3 treats then it updates the text and takes away the colliders so the dog cant recieve any more treats 
-            {
-                textManager.SetText("Enough, Stop");
-                // Disable the collider to prevent further collisions
-                Destroy(treatCollider);
-                Destroy(treatCollider2);
-                treatCollider.enabled = false;
-                treatCollider2.enabled = false;
-                return; // Exit the method to prevent further execution
-            }
+            treatCounter++; // Increment the treat counter
+            Debug.Log("Treat counter is now" + treatCounter);
 
-              
-            Destroy(gameObject); //This destroys the treat for the dog
-            textManager.SetText("Bark Bark"); 
-            scoreManager.IncrementScore(); //Incrementing the score that allows the scene to change
+            if (treatCounter == 3) // If the dog has received 3 treats 
+            {
+                scoreManager.IncrementScore();
+                textManager.SetText("Enough, Stop");
+                //Destroying the treat that it ate
+                Destroy(gameObject);
+                //This is where I disable all the colliders for every object that has this script on it 
+                treatCapsuleCollider.enabled = false;
+                treatSphereCollider.enabled = false;
+            }
+            else
+            {
+                
+                Destroy(gameObject);
+                textManager.SetText("Bark Bark");
+                scoreManager.IncrementScore();
+            }
         }
-    } 
- 
+    }
+
+
+
 }
